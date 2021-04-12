1b0ab247f650b71332bdbc13af926adef580562c
libobo1112006 <libobo1112006@users.noreply.github.com>
2021-04-12 11:05:10 +0800

    Update language-mapping-list.js
    rename 中文（中国）->中文（中国大陆）

--- language-mapping-list.js
+++ language-mapping-list.js
@@ -863,7 +863,7 @@
       englishName: "Chinese Traditional"
     },
     'zh-CN': {
-      nativeName: "中文（中国）",
+      nativeName: "中文（中国大陆）",
       englishName: "Chinese Simplified (China)"
     },
     'zh-HK': {
